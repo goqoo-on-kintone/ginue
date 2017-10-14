12c269d1c2af29b23b5a1fd323fced44731b1caf
Hisaki Akaza <TheRed@users.noreply.github.com>
2017-10-14 15:44:29 +0900

    requestからrequest-promiseに変更

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 #!/usr/bin/env node
 'use strict'
 
-const request = require('request')
+const rp = require('request-promise')
 
 const pretty = (obj) => JSON.stringify(obj, null, '    ')
 
-const argv = process.argv.slice(2)
-const url = argv[0]
-const base64account = argv[1]
-const headers = {
-  'X-Cybozu-Authorization': base64account
-}
-const options = { url, headers }
-request.get(options, (error, res, body) => {
-  if (error) {
-    console.error(error)
-    return
+const main = async () => {
+  const argv = process.argv.slice(2)
+  const url = argv[0]
+  const base64account = argv[1]
+  const headers = {
+    'X-Cybozu-Authorization': base64account
   }
-  const bodyObj = JSON.parse(body)
-  console.log(pretty(bodyObj))
-})
+  const options = { url, headers, json: true }
+  try {
+    const resp = await rp(options)
+    console.log(pretty(resp))
+  } catch (error) {
+    console.error(error)
+  }
+}
+main()
