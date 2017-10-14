f4a5f41ab02e36d9b6e9bdf5d88c31239b9f3031
Hisaki Akaza <TheRed@users.noreply.github.com>
2017-10-14 14:39:00 +0900

    最低限のGET処理を実装

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 #!/usr/bin/env node
 'use strict'
 
-console.log('hello ginue!')
+const request = require('request')
+
+const argv = process.argv.slice(2)
+const url = argv[0]
+const base64account = argv[1]
+const headers = {
+  'X-Cybozu-Authorization': base64account
+}
+const options = { url, headers }
+request.get(options, (error, res, body) => {
+  if (error) {
+    console.error(error)
+    return
+  }
+  console.log(body)
+})
