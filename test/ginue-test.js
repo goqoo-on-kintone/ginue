9c0ec4457398bad31dcb2afa8d40625dbbe3f7ad
Hisaki Akaza <TheRed@users.noreply.github.com>
2018-03-14 14:05:35 +0900

    test: テスト共用のhelper.jsを追加

--- test/ginue-test.js
+++ test/ginue-test.js
@@ -1,6 +1,4 @@
-const { it } = require('mocha')
-const assert = require('power-assert')
-const ginue = require('../lib/ginue')
+require('./helper.js')
 
 it('trimで先頭・末尾の改行が正しく削られる', function () {
   const newlineIncludedStr = `
